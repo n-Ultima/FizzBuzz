42918672e08b983c0046dce43cb2af5258bfdc78
Nicolas Barclay <78455052+n-Ultima@users.noreply.github.com>
2021-05-31 23:21:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+# Declare the for loop
 for fizzbuzz in range(101): # Iterates through the numbers up to 101.
+# If the current number divided by 3's remainder is 0, then we use "Fizz"
   if fizzbuzz % 3 == 0:
     print('Fizz')
+# If the current number divided by 5's remainder is 0, then we use "Buzz"
   if fizzbuzz % 5 == 0:
     print('Buzz')
+# If both divided by 3, and divided by 5, the remainder is 0, then we use "FizzBuzz"
   if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
     print ('FizzBuzz')
+# Loop
   else:
     print (fizzbuzz)
